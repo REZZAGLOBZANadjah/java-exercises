923973582e14f8a3d6f3851dff54e87f05aa286b
REZZAG LOBZA Nadjah <rezzaglobza7@gmail.com>
2020-11-03 11:35:47 +0100

    Update sum_two_numbers.java

--- sum_two_numbers.java
+++ sum_two_numbers.java
@@ -5,12 +5,26 @@ Expected Output :
 110
 */
 //Solution 02
-public class Main {
+/*public class Main {
   public static void main(String[] args) {
 	int x=74;
 	int y=+ 36;
 	System.out.println(" Test Data:\n"+x+ "+"+y );
 	int Output =x+y;
  	System.out.println("Expected Output :\n " +Output);
+}
+}
+*/
+//Other solution
+import java.util.Scanner;
+public class Main {
+  public static void main(String[] args) {
+    	Scanner input = new Scanner (System.in);
+        System.out.print("Input the first number: ");
+    	int num1 = input.nextInt();
+    	System.out.print("Input the second number: ");
+    	int num2=input.nextInt();
+    	int Output =num1+num2;
+    	System.out.println("Expected Output :\n " +Output);
 }
 }
