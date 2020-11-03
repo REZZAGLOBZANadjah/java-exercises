9d8e7d138a629d7acb28f1ce6bea48f9fb1ce641
REZZAG LOBZA Nadjah <rezzaglobza7@gmail.com>
2020-11-03 11:33:27 +0100

    Create sum_two_numbers.java

---
+++ sum_two_numbers.java
@@ -0,0 +1,16 @@
+/*2. Write a Java program to print the sum of two numbers. 
+Test Data:
+74 + 36
+Expected Output :
+110
+*/
+//Solution 02
+public class Main {
+  public static void main(String[] args) {
+	int x=74;
+	int y=+ 36;
+	System.out.println(" Test Data:\n"+x+ "+"+y );
+	int Output =x+y;
+ 	System.out.println("Expected Output :\n " +Output);
+}
+}
