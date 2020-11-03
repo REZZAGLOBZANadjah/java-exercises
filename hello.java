9fad1098d693c21d5b2fa7415aa130fb62f1f5cb
REZZAG LOBZA Nadjah <rezzaglobza7@gmail.com>
2020-11-03 11:31:19 +0100

    Create hello.java

---
+++ hello.java
@@ -0,0 +1,10 @@
+//https://www.w3resource.com/java-exercises/basic/index.php
+/*1. Write a Java program to print 'Hello' on screen and then print your name on a separate line. Go to the editor
+Expected Output :
+Hello REZZAG LOBZA Nadjah
+*/
+//Solution 01
+public class Main {
+  public static void main(String[] args) {
+  System.out.println("Hello\nREZZAG LOBZA Nadjah!");
+ }}
